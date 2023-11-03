14fcb5b775a6df11d60f6e7272d02e588cc97d90
jockerlol <jockerlol1@gmail.com>
2023-11-03 22:18:44 +0200

    second commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 from main import app
 from src.db.models import Base
-from src.db.db_connect import get_db
+from src.db.db_connection import get_db
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testDB.db"
 
